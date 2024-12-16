7d98419b302d241e933f52f77bbb79476463e235
steven2k2 <steven.thompson2k24@gmail.com>
2024-12-16 15:39:24 +1100

    feature: eBay page.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
     const page = pages.get(pageKey);
 
     if (page) {
-        res.locals.title = page.title;
+        res.locals.title = page.title || 'MISSING TITLE';
         res.locals.subtitle = page.subtitle || '';
         res.locals.keywords = page.keywords || '';
         res.locals.description = page.description || '';
